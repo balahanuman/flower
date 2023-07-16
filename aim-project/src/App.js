3653b16ec40c84a3c6e350687c738579688b2701
sunitha <mandlasunitha60@gmail.com>
2023-07-16 17:49:17 +0530

    react project

---
+++ aim-project/src/App.js
@@ -0,0 +1,194 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div className='container-fluid'>
+       <div className='row'>
+      <div className='col-md-3'>
+       
+        <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.6lL4MboZ6-ZtULpHuGykaAHaE7&pid=Api&rs=1&c=1&qlt=95&w=161&h=107'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+             </div>  
+           </div> 
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.1YM53mG10H_U25iPjop83QHaEo&pid=Api&rs=1&c=1&qlt=95&w=187&h=117'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.roeJGz3eeyhxK3XRQ0LxpQHaFj&pid=Api&rs=1&c=1&qlt=95&w=156&h=117'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.Z3iF66QpQVHe8HkLPj-RggHaEo&pid=Api&rs=1&c=1&qlt=95&w=187&h=117'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.xdQTJ2OEqbcncyUA7_Ke0wHaE8&pid=Api&rs=1&c=1&qlt=95&w=151&h=100'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.HxV79tFMPfBAIo0BBF-sOgHaEy&pid=Api&rs=1&c=1&qlt=95&w=156&h=100'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.ENHVKJlP4msrDSCvciIAXwHaE8&pid=Api&rs=1&c=1&qlt=95&w=151&h=100'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.cfxVoPK-zXAsxKiTmA3qVwHaFj&pid=Api&rs=1&c=1&qlt=95&w=135&h=101'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.0WSwZk0xz4jmE-WGeaBjzwHaEL&pid=Api&rs=1&c=1&qlt=95&w=179&h=101'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.YDJzemAv0ZM5JTksh6zsqAHaF7&pid=Api&rs=1&c=1&qlt=95&w=127&h=102'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.b6xvO40scdFgxa8JAcBp-QHaFj&pid=Api&rs=1&c=1&qlt=95&w=136&h=102'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.c4MCiDFgSGLsR_7-4-j0PwHaEK&pid=Api&rs=1&c=1&qlt=95&w=182&h=102'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.UHbAF6yPon33W3A03yO47wHaFj&pid=Api&rs=1&c=1&qlt=95&w=143&h=107'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.J85iWsx_agdzpOJuQQnlpAHaFj&pid=Api&rs=1&c=1&qlt=95&w=143&h=107'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.MCLzVoExgXPyNi_V5gb1AwHaE7&pid=Api&rs=1&c=1&qlt=95&w=161&h=107'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          <div className='col-md-3'>
+          <div className='card my-5'>
+          <img src='https://tse1.mm.bing.net/th?id=OIP.6lL4MboZ6-ZtULpHuGykaAHaE7&pid=Api&rs=1&c=1&qlt=95&w=161&h=107'alt='' className='img-card-top'/>
+          <div className='card-body'>
+           <h3></h3> 
+           <p></p>
+           <a href='' className='btn btn-primary'>Add to contact</a>
+               
+           </div> 
+          </div>
+          </div>
+          
+
+          </div>
+          </div>
+          </> 
+        
+          
+  )
+}
+
+export default App
